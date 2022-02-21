c0d9d24e94f508ce078d569de01854a39f6ed9fd
Nihad19 <86147986+Nihad18@users.noreply.github.com>
2022-02-21 21:33:49 +0400

    Add files via upload

---
+++ interior-consultant-master/assets/app.js
@@ -0,0 +1,5 @@
+const button=document.getElementById('button');
+const navUl=document.getElementById('navUl');
+button.addEventListener("click", function () {
+ navUl.classList.toggle("nav-active")
+});
